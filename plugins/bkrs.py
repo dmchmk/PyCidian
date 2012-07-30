d1482af18cf590be5738b34ed89ecde0620efb5e
Dmitry Chumak <mega.venik@gmail.com>
2012-07-30 17:55:10 +0900

    Getting of found words seems to be working properly now

--- plugins/bkrs.py
+++ plugins/bkrs.py
@@ -15,16 +15,14 @@ def word_search(word):
     if doc.xpath(BASE_PATH + 'div[@id="ajax_search"]/descendant::div[@class="margin_left"]/noindex/div[@class="green pt12"]/text()') == [u'Такого слова нет ']:
         WORD_NOT_FOUND = u'Такого слова нет'
 
-    #print f.read()
-    #f = urllib2.urlopen('http://bkrs.info/slovo.php?ch=遥远工作')
-    #f = urllib2.urlopen('http://bkrs.info/slovo.php?ch=遥远')
-
-    chin = ""
-    rus = ""
-    #table = ""
-
+    translate = u''
+    word = word.decode('utf-8')
     if WORD_NOT_FOUND:
+        #print "Word not found"
         num = 1
+        #print word, WORD_NOT_FOUND
+        translate += '[=== %s: %s ===]\n\n' % (word, WORD_NOT_FOUND)
+        #print '\n'
         while True:
             chinese = doc.xpath('%sdiv[@id="ajax_search"]/div[@class="margin_left"]/table/tr[@class="vtop"]/td[%s]/div[@class="ch2"]/a[@class="black"]/text()' % (BASE_PATH, num))
             pinyin = doc.xpath('%sdiv[@id="ajax_search"]/div[@class="margin_left"]/table/tr[@class="vtop"]/td[%s]/div[@class="py2"]/text()' % (BASE_PATH, num))
@@ -32,22 +30,39 @@ def word_search(word):
             examples = doc.xpath('%sdiv[@id="ajax_search"]/div[@class="margin_left"]/table/tr[@class="vtop"]/td[%s]/div[@class="ru"]/div[@class="m2"]/div[@class="ex"]/text()' % (BASE_PATH, num))
             for ch in chinese:
                 for py in pinyin:
-                    print ch, py
+                    #print ch, py
+                    translate += '-- %s %s --\n' % (ch, py)
                     for ru in russian:
-                        print ru
+                        #print ru
+                        translate += '%s\n' % (ru)
                     #print '\n'
-                    print 'Примеры:'
+                    #print 'Примеры:'
+                    translate += '\nExamples:\n'
                     for ex in examples:
-                        print ex
+                        #print ex
+                        translate += '%s\n' % (ex)
             num += 1
             if not russian:
                 break
-            print '\n'
+            #print '\n'
+            translate += '\n'
+            #print '====================================='
+            #print translate
     else:
-        chin = doc.xpath(BASE_PATH + 'div[@id="ch"]/text()')
-        #rus = doc.xpath(BASE_PATH + 'div[@class="ru"]/text()')
-        rus = doc.xpath(BASE_PATH + 'div[@class="ru"]/div/text()')
+        print "Word found"
+        pinyin = doc.xpath(BASE_PATH + 'div[@class="py"]/text()')
+        rus_single = doc.xpath(BASE_PATH + 'div[@class="ru"]/text()') # checking whether searched word has many meanings or not. If it is, we show rus_full, if it's not, we show rus_single
+        rus_full = doc.xpath(BASE_PATH + 'div[@class="ru"]/*[not(@class="ex")]')
+        examples = doc.xpath(BASE_PATH + 'div[@class="ru"]/div[@class="m2"]/div[@class="ex"]')
 
-        print "%s: %s" % (word, WORD_NOT_FOUND)
-        for i in rus:
-            print i
+        print '%s: %s' % (word, pinyin[0].strip())
+
+        if rus_single[0].strip():
+            print rus_single[0]
+        else:
+            #print "No rus"
+            for ru in rus_full:
+                print ru.text_content()
+            for ex in examples:
+                print ex.text
+    print translate.encode('utf-8')
