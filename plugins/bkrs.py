fef2d2fd0fae4dd01ddaa86738bc11ddf5cb7c31
Dmitry Chumak <mega.venik@gmail.com>
2012-09-05 12:52:35 +0900

    a) XPath's search paths were cutted pretty much b) Separate "examples" loop deleted due to unnecessity. c) All translations are now getting by "text_content()" directive.

--- plugins/bkrs.py
+++ plugins/bkrs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 #-*- coding: utf-8 -*-
 
 import urllib2 # in python3 urllib2 was splited
@@ -16,56 +16,46 @@ def word_search(word):
     translate = u''
     word = word.decode('utf-8')
     if WORD_NOT_FOUND:
-        #print "Word not found"
+        ### WORD NOT FOUND
         num = 1
-        #print word, WORD_NOT_FOUND
         translate += '[=== %s: %s ===]\n\n' % (word, WORD_NOT_FOUND)
-        #print '\n'
         while True:
-            chinese = doc.xpath('//div[@id="ajax_search"]/div[@class="margin_left"]/table/tr[@class="vtop"]/td[%s]/div[@class="ch2"]/a[@class="black"]/text()' % (num))
-            pinyin = doc.xpath('//div[@id="ajax_search"]/div[@class="margin_left"]/table/tr[@class="vtop"]/td[%s]/div[@class="py2"]/text()' % (num))
-            russian = doc.xpath('//div[@id="ajax_search"]/div[@class="margin_left"]/table/tr[@class="vtop"]/td[%s]/div[@class="ru"]/div/text()' % (num))
-            examples = doc.xpath('//div[@id="ajax_search"]/div[@class="margin_left"]/table/tr[@class="vtop"]/td[%s]/div[@class="ru"]/div[@class="m2"]/div[@class="ex"]/text()' % (num))
+            #chinese = doc.xpath('//div[@id="ajax_search"]/div[@class="margin_left"]/table/tr[@class="vtop"]/td[%s]/div[@class="ch2"]/a[@class="black"]/text()' % (num))
+            #pinyin = doc.xpath('//div[@id="ajax_search"]/div[@class="margin_left"]/table/tr[@class="vtop"]/td[%s]/div[@class="py2"]/text()' % (num))
+            #russian = doc.xpath('//div[@id="ajax_search"]/div[@class="margin_left"]/table/tr[@class="vtop"]/td[%s]/div[@class="ru"]/div' % (num))
+            chinese = doc.xpath('//td[%s]/div[@class="ch2"]/a[@class="black"]/text()' % (num))
+            pinyin = doc.xpath('//td[%s]/div[@class="py2"]/text()' % (num))
+            russian = doc.xpath('//td[%s]/div[@class="ru"]/div' % (num))
+            #examples = doc.xpath('//div[@id="ajax_search"]/div[@class="margin_left"]/table/tr[@class="vtop"]/td[%s]/div[@class="ru"]/div[@class="m2"]/div[@class="ex"]/text()' % (num))
             for ch in chinese:
                 for py in pinyin:
-                    #print ch, py
                     translate += '-- %s %s --\n' % (ch, py)
                     for ru in russian:
-                        #print ru
-                        ### TODO: Add `text_content()` to get all the contents of the russian translation
-                        translate += '%s\n' % (ru)
-                    #print '\n'
-                    #print 'Примеры:'
-                    translate += '\nExamples:\n'
-                    for ex in examples:
-                        #print ex
-                        translate += '%s\n' % (ex)
+                        translate += '%s\n' % (ru.text_content())
+                    #if examples:
+                    #    translate += '\nExamples:\n'
+                    #    for ex in examples:
+                    #        translate += '%s\n' % (ex)
             num += 1
             if not russian:
                 break
-            #print '\n'
             translate += '\n'
-            #print '====================================='
-            #print translate
     else:
-        print "Word found"
+        ### WORD FOUND
         pinyin = doc.xpath('//div[@class="py"]/text()')
-        rus_single = doc.xpath('//div[@class="ru"]/text()') # checking whether searched word has many meanings or not. If it is, we show rus_full, if it's not, we show rus_single
+        rus_single = doc.xpath('//div[@class="ru"]') # checking whether searched word has many meanings or not. If it is, we show rus_full, if it's not, we show rus_single
         rus_full = doc.xpath('//div[@class="ru"]/*[not(@class="ex")]')
-        examples = doc.xpath('//div[@class="ru"]/div[@class="m2"]/div[@class="ex"]')
+        #examples = doc.xpath('//div[@class="ru"]/div[@class="m2"]/div[@class="ex"]')
 
-        #print u'%s: %s' % (word, pinyin[0].strip())
         translate += u'%s: %s\n' % (word, pinyin[0].strip())
 
-        if rus_single[0].strip():
-            #print rus_single[0]
-            translate += '%s\n' % (rus_single[0])
+        if len(rus_single[0]):
+            translate += '%s\n' % (rus_single[0].text_content())
         else:
-            #print "No rus"
             for ru in rus_full:
-                #print ru.text_content()
                 translate += '%s\n' % (ru.text_content())
-            for ex in examples:
-                #print ex.text
-                translate += '%s' % (ex.text)
+            #if examples:
+            #    translate += '\nExamples:\n'
+            #    for ex in examples:
+            #        translate += '%s\n' % (ex.text_content())
     print translate.encode('utf-8')
