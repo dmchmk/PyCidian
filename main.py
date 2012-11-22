bd8488b744a8bb175b181fa303a305238d8d7e0b
Dmitry Chumak <mega.venik@gmail.com>
2012-11-22 20:49:09 +0900

    Adding Zhonga module

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 #-*- coding: utf-8 -*-
 
 import sys, urllib2
-from plugins import bkrs
+#from plugins import bkrs
+from plugins import zhonga
 
+string = u''
 string = sys.stdin.read()
 
-#f = urllib2.urlopen('http://bkrs.info/slovo.php?ch=%s' % string)
+f = urllib2.urlopen('http://bkrs.info/slovo.php?ch=%s' % string)
 #print f.read()
 
-bkrs.word_search(string.strip('\n'))
+#f = urllib2.urlopen('http://bkrs.info/slovo.php?ch=遥远工作')
+#f = urllib2.urlopen('http://bkrs.info/slovo.php?ch=遥远')
+
+#bkrs.word_search(string.strip('\n'))
+zhonga.word_search(string.strip('\n'))
 
 #print string.strip('\n')
